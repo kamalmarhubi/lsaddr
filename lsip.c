fa622c68c38829cc75bb5c4dd830a3a7f006dc4f
Kamal Marhubi <kamal@marhubi.com>
2015-06-15 11:39:17 -0400

    Run clang-format and add .clang-format

--- lsip.c
+++ lsip.c
@@ -15,43 +15,47 @@
 #endif
 
 int main() {
-    if (access("/proc/net", R_OK)) {
-        goto errorout;
-    }
-    if (access("/proc/net/if_inet6", R_OK)) {
-        goto errorout;
-    }
-    int ipv4_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    if (ipv4_fd == -1) {
-        goto errorout;
-    }
+  if (access("/proc/net", R_OK)) {
+    goto errorout;
+  }
+  if (access("/proc/net/if_inet6", R_OK)) {
+    goto errorout;
+  }
+  int ipv4_fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+  if (ipv4_fd == -1) {
+    goto errorout;
+  }
 
-    struct ifconf stuff;
-    stuff.ifc_len = 0;
-    stuff.ifc_buf = NULL;
+  struct ifconf stuff;
+  stuff.ifc_len = 0;
+  stuff.ifc_buf = NULL;
 
-    if (ioctl(ipv4_fd, SIOCGIFCONF, &stuff)) {
-        goto errorout;
-    }
-    if (!(stuff.ifc_buf = malloc(stuff.ifc_len))) {
-        goto errorout;
-    }
-    if (ioctl(ipv4_fd, SIOCGIFCONF, &stuff)) {
-        goto errorout;
-    }
+  if (ioctl(ipv4_fd, SIOCGIFCONF, &stuff)) {
+    goto errorout;
+  }
+  if (!(stuff.ifc_buf = malloc(stuff.ifc_len))) {
+    goto errorout;
+  }
+  if (ioctl(ipv4_fd, SIOCGIFCONF, &stuff)) {
+    goto errorout;
+  }
 
-    int i, len;
-    char addr[BUF_LEN];
+  int i, len;
+  char addr[BUF_LEN];
 
-    for (i = 0, len = stuff.ifc_len / sizeof(struct ifreq); i < len; ++i) {
-        if (!strncmp(stuff.ifc_req[i].ifr_name, "lo", 2)) { continue; }
-        struct sockaddr_in *sockaddr =  (struct sockaddr_in*) &stuff.ifc_req[i].ifr_addr;
-        printf("%s\t%s\n", inet_ntop(AF_INET, &sockaddr->sin_addr, addr, BUF_LEN), stuff.ifc_req[i].ifr_name);
+  for (i = 0, len = stuff.ifc_len / sizeof(struct ifreq); i < len; ++i) {
+    if (!strncmp(stuff.ifc_req[i].ifr_name, "lo", 2)) {
+      continue;
     }
+    struct sockaddr_in *sockaddr =
+        (struct sockaddr_in *)&stuff.ifc_req[i].ifr_addr;
+    printf("%s\t%s\n", inet_ntop(AF_INET, &sockaddr->sin_addr, addr, BUF_LEN),
+           stuff.ifc_req[i].ifr_name);
+  }
 
-    return 0;
+  return 0;
 
 errorout:
-    perror("something went wrong");
-    exit(EXIT_FAILURE);
+  perror("something went wrong");
+  exit(EXIT_FAILURE);
 }
