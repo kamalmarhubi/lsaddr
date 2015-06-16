8f445878c019d4e2b690b941b30f2e7e455962d4
Kamal Marhubi <kamal@marhubi.com>
2015-06-16 15:20:38 -0400

    Add argument parsing with argp
    This adds argument parsing and updates the man page source to match.

--- lsaddr.c
+++ lsaddr.c
@@ -1,3 +1,4 @@
+#include <argp.h>
 #include <arpa/inet.h>
 #include <net/if.h>
 #include <netinet/in.h>
@@ -14,7 +15,75 @@
 #define BUF_LEN INET6_ADDRSTRLEN
 #endif
 
-int main() {
+/* Options without short options */
+#define OPT_INCLUDE_LOOPBACK 1
+#define OPT_INCLUDE_LINK_LOCAL 2
+
+static struct argp_option options[] = {
+    {"ipv4", '4', 0, 0,
+     "List IPv4 addresses",
+     0},
+    {"ipv6", '6', 0, 0,
+     "List IPv6 addresses",
+     0},
+    {"include-loopback", OPT_INCLUDE_LOOPBACK, 0, 0,
+     "Include addresses for the loopback interface",
+     0},
+    {"include-link-local", OPT_INCLUDE_LINK_LOCAL, 0, 0,
+     "Include IPv6 link-local addresses",
+     0},
+    {0}};
+
+/* Command line options */
+struct args {
+  /* The union is a little hack to allow setting either ipv4 or ipv6 to result
+   * in ip_version_specified being true. */
+  union {
+    int ip_version_specified;
+    struct {
+      int ipv4 : 1;
+      int ipv6 : 1;
+    };
+  };
+  int include_loopback;
+  int include_link_local;
+  char **interfaces;
+  int num_interfaces;
+};
+
+static error_t parse_opt(int key, char *arg __attribute__((unused)), struct argp_state *state) {
+  struct args *args = state->input;
+
+  switch (key) {
+    case '4':
+      args->ipv4 = 1;
+      break;
+    case '6':
+      args->ipv6 = 1;
+      break;
+    case ARGP_KEY_ARGS:
+      args->interfaces = state->argv + state->next;
+      args->num_interfaces = state->argc - state->next;
+      break;
+    default:
+      return ARGP_ERR_UNKNOWN;
+  }
+  return 0;
+}
+
+static struct argp argp = {options, parse_opt, 0, 0, 0, 0, 0};
+
+int main(int argc, char **argv) {
+  struct args args = {
+    .ip_version_specified = 0,  /* also sets ipv4 and ipv6 to 0 */
+    .include_loopback = 0,
+    .include_link_local = 0,
+    .interfaces = NULL,
+    .num_interfaces = 0,
+  };
+
+  argp_parse(&argp, argc, argv, 0, 0, &args);
+
   if (access("/proc/net", R_OK)) {
     goto errorout;
   }
